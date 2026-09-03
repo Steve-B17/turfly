a3136d7854d780ce82b4ee4c9a929b3b4ac91e17
Steve-B17 <steve.joel.cbe@gmail.com>
2026-09-03 14:00:34 +0530

    Final Commit

--- src/main/java/com/example/turfly/TurflyApplication.java
+++ src/main/java/com/example/turfly/TurflyApplication.java
@@ -2,12 +2,13 @@ package com.example.turfly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class TurflyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TurflyApplication.class, args);
 	}
-
 }
