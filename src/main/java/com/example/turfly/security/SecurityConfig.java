140a09064ea1d75804bb0c32cf8c3f6876384183
Steve-B17 <steve.joel.cbe@gmail.com>
2026-09-03 15:49:43 +0530

    Final Commit

--- src/main/java/com/example/turfly/security/SecurityConfig.java
+++ src/main/java/com/example/turfly/security/SecurityConfig.java
@@ -54,7 +54,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://turfly-frontend.vercel.app/"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
